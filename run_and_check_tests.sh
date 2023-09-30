8ed4156282857f0610e88f9a71958167412be29f
Eirik Kolås <kolaseirik@gmail.com>
2023-09-30 14:50:12 +0200

    test action

---
+++ run_and_check_tests.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# This script runs the unit tests and checks the results.
+declare file="test_results"
+declare regex=" tests, 0 errors, 0 failures, 0 skipped"
+
+# Source ROS2
+. /opt/ros/humble/setup.bash
+
+# Build the package
+colcon build
+
+# Run the unit tests
+colcon test
+
+# Check the results
+colcon test-result > "${file}"
+
+declare file_content=$( cat "${file}" )
+if [[ " $file_content " =~ $regex ]]; then
+    echo "Unit tests passed"
+    exit 0
+else
+    echo "Unit tests failed"
+    exit 1
+fi
